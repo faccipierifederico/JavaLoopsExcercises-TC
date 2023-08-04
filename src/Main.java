6bcfc13904aadaad25934203484f4f72241e6037
Federico Faccipieri <federicofaccipieri@gmail.com>
2023-08-04 15:18:04 -0300

    1st & 2nd excercises

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // EXC 1: Realizar un programa que dado por teclado un límite numérico muestre por pantalla todos los números hasta dicho límite
+
+/*        System.out.println("Choose a number to apply a limit for the aplication: ");
+        Scanner scanner = new Scanner(System.in);
+        int limit = scanner.nextInt();
+        int count = 1;
+
+        while (count <= limit) {
+            System.out.println("The count is: " + count);
+            count = count + 1;
+        }*/
+
+        // EXC 2: Realizar un programa que muestre en pantalla palbras que sean ingresadas por teclado hasta que se ingrese la palabra "salir".
+
+/*        System.out.println("Type a word to introduce it in the application");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+
+        while (!word.equalsIgnoreCase("salir")) {
+            System.out.println("The word you write is: " + word);
+            System.out.println("Type another word to introduce it in the application");
+            word = scanner.nextLine();
+        }*/
+
+    }
+}
