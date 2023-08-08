63d04e768f47bdad6d14af3a5999f6ef6f58af9e
Federico Faccipieri <federicofaccipieri@gmail.com>
2023-08-08 14:15:56 -0300

    3rd excercise done.

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,51 @@ public class Main {
             word = scanner.nextLine();
         }*/
 
+        // Dada una maratón, realizar un programa que el fin sea informarle a los posibles participantes a qué categoría pertenecen. Para ello,
+        // pedir los siguientes datos: dni, nombre y edad; de acuerdo a ello, mostrar por pantalla la categoría correspondiente.
+        // Las categorías son: Menores A (de 6 a 10); Menores B (de 11 a 17); Juveniles (de 18 a 30); Adultos (31 a 50); Adultos mayores (mayores de 50).
+        // Para finalizar el programa ingresar un valor de DNI 0 y nombre con la palabra "fin".
+
+        System.out.println("Hi! Write your DNI: ");
+        Scanner scannerDni = new Scanner(System.in);
+        String dni = scannerDni.nextLine();
+        System.out.println("Now write your first name:");
+        Scanner scannerName = new Scanner(System.in);
+        String name = scannerName.nextLine();
+        System.out.println("Write your last name:");
+        Scanner scannerLastName = new Scanner(System.in);
+        String lastName = scannerLastName.nextLine();
+        System.out.println("And finally, tell me your age:");
+        Scanner scannerAge = new Scanner(System.in);
+        int age = scannerAge.nextInt();
+
+        while (!dni.equals("0") && (!name.equalsIgnoreCase("fin"))) {
+            if (age < 6) {
+                System.out.println("I'm sorry. You are too little to join the tournament.");
+            } else if (age >= 6 && age <= 10) {
+                System.out.println("You have to subscribe por this category: 'Menores A'");
+            } else if (age >= 11 && age <= 17) {
+                System.out.println("You have to subscribe por this category: 'Menores B'");
+            } else if (age >= 18 && age <= 30) {
+                System.out.println("You have to subscribe por this category: 'Juveniles'");
+            } else if (age >= 31 && age <= 50) {
+                System.out.println("You have to subscribe por this category: 'Adultos'");
+            } else if (age > 50 && age < 120) {
+                System.out.println("You have more than 50 years. Subscribe to: 'Adultos mayores'");
+            } else {
+                System.out.println("You write a wrong age. Please, do it again.");
+            }
+
+            System.out.println("Hi! Write your DNI: ");
+            dni = scannerDni.nextLine();
+            System.out.println("Now write your first name:");
+            name = scannerName.nextLine();
+            System.out.println("Write your last name:");
+            lastName = scannerLastName.nextLine();
+            System.out.println("And finally, tell me your age:");
+            age = scannerAge.nextInt();
+        }
+
+        System.out.println("You finished the application. Good bye!");
     }
 }
